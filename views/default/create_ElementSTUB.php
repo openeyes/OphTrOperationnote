e09db7517feb4a89f2c479fb4ca6b0d2e9a372ab
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-04-16 15:36:19 +0100

    fixed ondemand border around the STUB element views.

--- views/default/create_ElementSTUB.php
+++ views/default/create_ElementSTUB.php
@@ -18,7 +18,7 @@
  */
 ?>
 
-<div class="<?php echo $element->elementType->class_name?>">
+<div class="<?php echo $element->elementType->class_name?> ondemand<?php if (@$ondemand){?> hidden<?php }?>">
 	<h4 class="elementTypeName"><?php echo $element->elementType->name ?></h4>
 	<?php echo $form->textArea($element, 'comments', array('rows' => 4, 'cols' => 60))?>
 </div>
