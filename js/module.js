4933bb48fd7226e0eb88da1deb5277ee52693b1f
Mark Wadham <m@rkw.me>
2012-05-01 17:24:24 +0100

    new save/cancel button functionality.

--- js/module.js
+++ js/module.js
@@ -79,6 +79,27 @@ function setCataractInput(key, value) {
 }
 
 $(document).ready(function() {
+	$('#et_save').unbind('click').click(function() {
+		if (!$(this).hasClass('inactive')) {
+			disableButtons();
+			return true;
+		}
+		return false;
+	});
+
+	$('#et_cancel').unbind('click').click(function() {
+		if (!$(this).hasClass('inactive')) {
+			disableButtons();
+
+			if (m = window.location.href.match(/\/update\/[0-9]+/)) {
+				window.location.href = window.location.href.replace('/update/','/view/');
+			} else {
+				window.location.href = '/patient/episodes/'+et_patient_id;
+			}
+		}
+		return false;
+	});
+
 	$("button[id$='_generate_report']").unbind('click').click(function(e) {
 		e.preventDefault();
 
