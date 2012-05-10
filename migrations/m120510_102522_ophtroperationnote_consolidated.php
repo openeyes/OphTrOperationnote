04332b0b2b4157f6588a9b20f1298f38f457ba40
Mark Wadham <m@rkw.me>
2012-05-10 11:25:45 +0100

    re-consolidated migrations.

--- migrations/m120510_102522_ophtroperationnote_consolidated.php
+++ migrations/m120510_102522_ophtroperationnote_consolidated.php
@@ -1,5 +1,5 @@
 <?php
-class m120417_113857_ophtroperationnote_consolidated extends CDbMigration
+class m120510_102522_ophtroperationnote_consolidated extends CDbMigration
 {
 	public function up() {
 				// create et_ophtroperationnote_procedurelist
@@ -7095,9 +7095,67 @@ Aspiration of soft lens matter'");
 			$this->insert('et_ophtroperationnote_site_subspecialty_postop_instructions',array('id'=>1,'site_id'=>1,'subspecialty_id'=>$subspecialty['id'],'content'=>'Use drops three times a day','display_order'=>1));
 			$this->insert('et_ophtroperationnote_site_subspecialty_postop_instructions',array('id'=>2,'site_id'=>1,'subspecialty_id'=>$subspecialty['id'],'content'=>'Use drops four times a day','display_order'=>2));
 		}
+			$event_type = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('name=:name', array(':name'=>'Operation note'))->queryRow();
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Procedure list'))->queryRow();
+		$this->update('element_type',array('display_order'=>1),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Vitrectomy'))->queryRow();
+		$this->update('element_type',array('display_order'=>2),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Membrane peel'))->queryRow();
+		$this->update('element_type',array('display_order'=>2),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Tamponade'))->queryRow();
+		$this->update('element_type',array('display_order'=>2),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Buckle'))->queryRow();
+		$this->update('element_type',array('display_order'=>2),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Cataract'))->queryRow();
+		$this->update('element_type',array('display_order'=>2),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Anaesthetic'))->queryRow();
+		$this->update('element_type',array('display_order'=>3),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Surgeon'))->queryRow();
+		$this->update('element_type',array('display_order'=>4),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Post-op drugs'))->queryRow();
+		$this->update('element_type',array('display_order'=>5),'id='.$element_type['id']);
 	}
 
 	public function down() {
+	
+		$event_type = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('name=:name', array(':name'=>'Operation note'))->queryRow();
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Procedure list'))->queryRow();
+		$this->update('element_type',array('display_order'=>1),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Vitrectomy'))->queryRow();
+		$this->update('element_type',array('display_order'=>2),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Membrane peel'))->queryRow();
+		$this->update('element_type',array('display_order'=>3),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Tamponade'))->queryRow();
+		$this->update('element_type',array('display_order'=>4),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Buckle'))->queryRow();
+		$this->update('element_type',array('display_order'=>5),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Cataract'))->queryRow();
+		$this->update('element_type',array('display_order'=>6),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Anaesthetic'))->queryRow();
+		$this->update('element_type',array('display_order'=>7),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Surgeon'))->queryRow();
+		$this->update('element_type',array('display_order'=>4),'id='.$element_type['id']);
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:event_type_id and name=:name', array(':event_type_id'=>$event_type['id'],':name'=>'Post-op drugs'))->queryRow();
+		$this->update('element_type',array('display_order'=>5),'id='.$element_type['id']);
+	
 	
 		$this->dropTable('et_ophtroperationnote_site_subspecialty_postop_instructions');
 	
