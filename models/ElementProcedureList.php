390491236165b808955fcbcea9ab122862536504
Mark Wadham <m@rkw.me>
2012-05-25 16:10:13 +0100

    episode status rule.

--- models/ElementProcedureList.php
+++ models/ElementProcedureList.php
@@ -149,6 +149,11 @@ class ElementProcedureList extends BaseEventTypeElement
 			}
 		}
 
+		$this->event->episode->episode_status_id = 4;
+		if (!$this->event->episode->save()) {
+			throw new Exception('Unable to change episode status for episode '.$this->event->episode->id);
+		}
+
 		return parent::afterSave();
 	}
 
