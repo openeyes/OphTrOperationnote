ef3143a1799f0ff0e30b2c861c02f59f398deb48
Mark Wadham <m@rkw.me>
2012-06-19 09:49:08 +0100

    enforce length of incision between 0.1 - 9.9

--- js/module.js
+++ js/module.js
@@ -166,6 +166,11 @@ $(document).ready(function() {
 
 	$('#ElementCataract_length').die('change').live('change',function() {
 		if (doodle = magic.getDoodle('PhakoIncision')) {
+			if (parseFloat($(this).val()) > 9.9) {
+				$(this).val(9.9);
+			} else if (parseFloat($(this).val()) < 0.1) {
+				$(this).val(0.1);
+			}
 			doodle.setParameter('incisionLength',$(this).val());
 			magic.repaintCataract();
 		}
