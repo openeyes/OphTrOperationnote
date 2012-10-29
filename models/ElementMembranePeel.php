507f8f3037621eb926f011b2a00adbe160615ca9
Jamie Neil <jamie.neil@openeyes.org.uk>
2012-10-29 16:01:07 +0000

    Fix validation rules on membranepeel

--- models/ElementMembranePeel.php
+++ models/ElementMembranePeel.php
@@ -59,7 +59,8 @@ class ElementMembranePeel extends BaseEventTypeElement
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('event_id, membrane_blue, brilliant_blue, comments, other_dye', 'safe'),
+			array('membrane_blue, brilliant_blue', 'required'),
+			array('event_id, comments, other_dye', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
 			array('id, event_id, membrane_blue, brilliant_blue, other_dye', 'safe', 'on' => 'search'),
