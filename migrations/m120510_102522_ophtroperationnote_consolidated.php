572bd6cd4e8921d6fe957ad6d0f43dc0542e4cfc
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-02-11 17:45:12 +0000

    Fix watermark Fix migration error

--- migrations/m120510_102522_ophtroperationnote_consolidated.php
+++ migrations/m120510_102522_ophtroperationnote_consolidated.php
@@ -77,7 +77,7 @@ class m120510_102522_ophtroperationnote_consolidated extends CDbMigration
 				'KEY `procedure_id` (`proc_id`)',
 				'KEY `et_ophtroperationnote_plpa_last_modified_user_id_fk` (`last_modified_user_id`)',
 				'KEY `et_ophtroperationnote_plpa_created_user_id_fk` (`created_user_id`)',
-				'CONSTRAINT `et_ophtroperationnote_plpa_operation_fk` FOREIGN KEY (`procedurelist_id`) REFERENCES `element_operation` (`id`)',
+				'CONSTRAINT `et_ophtroperationnote_plpa_operation_fk` FOREIGN KEY (`procedurelist_id`) REFERENCES `et_ophtroperationnote_procedurelist` (`id`)',
 				'CONSTRAINT `et_ophtroperationnote_plpa_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
 				'CONSTRAINT `et_ophtroperationnote_plpa_ibfk_1` FOREIGN KEY (`proc_id`) REFERENCES `proc` (`id`)',
 				'CONSTRAINT `et_ophtroperationnote_plpa_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)'
