39c60863a282cded70b4887940fb17d99e3766a7
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-03-26 15:58:59 +0000

    Check canPrint

--- views/default/view.php
+++ views/default/view.php
@@ -23,7 +23,9 @@
 
 <?php
 	// Event actions
-	$this->event_actions[] = EventAction::button('Print', 'print');
+	if($this->canPrint()) {
+		$this->event_actions[] = EventAction::button('Print', 'print');
+	}
 	$this->renderPartial('//patient/event_actions');
 ?>
 
