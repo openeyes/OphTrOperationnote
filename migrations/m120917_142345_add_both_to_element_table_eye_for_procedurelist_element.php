5e2c378f44bd9c6b4c23503d373d98bc5c76f84f
Mark <m@rkw.me>
2013-08-07 15:37:17 +0100

    [OE-3301] bugfixes for when running against mysql with auto increment offsets

--- migrations/m120917_142345_add_both_to_element_table_eye_for_procedurelist_element.php
+++ migrations/m120917_142345_add_both_to_element_table_eye_for_procedurelist_element.php
@@ -8,7 +8,7 @@ class m120917_142345_add_both_to_element_table_eye_for_procedurelist_element ext
 		$element_type = ElementType::model()->find('event_type_id=? and class_name=?',array($event_type->id,'ElementProcedureList'));
 
 		$this->update('element_type_eye',array('display_order'=>3),'element_type_id='.$element_type->id.' and eye_id=1');
-		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>3,'display_order'=>2));
+		$this->insert('element_type_eye',array('element_type_id'=>$element_type->id,'eye_id'=>3,'display_order'=>2,'created_user_id'=>1,'last_modified_user_id'=>1));
 	}
 
 	public function down()
