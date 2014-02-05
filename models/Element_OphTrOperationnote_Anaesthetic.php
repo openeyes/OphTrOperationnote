c3078480ed7c48cf66a0898a4a3e26c9dc29631f
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-02-05 10:26:34 +0000

    [OEM-138] Remove default anaesthetic type.

--- models/Element_OphTrOperationnote_Anaesthetic.php
+++ models/Element_OphTrOperationnote_Anaesthetic.php
@@ -145,8 +145,6 @@ class Element_OphTrOperationnote_Anaesthetic extends BaseEventTypeElement
 	*/
 	public function setDefaultOptions()
 	{
-		$this->anaesthetic_type_id = 1;
-
 		if (Yii::app()->getController()->getAction()->id == 'create') {
 			if (!$patient = Patient::model()->findByPk(@$_GET['patient_id'])) {
 				throw new SystemException('Patient not found: '.@$_GET['patient_id']);
