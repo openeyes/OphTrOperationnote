d11d18405571f6c9ac257ff0dab60cf371ff5de0
Mike Smith <mike.smith@openeyes.org.uk>
2014-05-14 08:59:06 +0100

    + [OE-4009] utilise all OEMigration functions for migration

--- migrations/m140513_101425_add_trabectome_element.php
+++ migrations/m140513_101425_add_trabectome_element.php
@@ -4,6 +4,7 @@ class m140513_101425_add_trabectome_element extends OEMigration
 {
 	public function up()
 	{
+
 		$event_type_id = $this->insertOEEventType( 'Operation Note', 'OphTrOperationnote', 'Ci');
 		$et_ids = $this->insertOEElementType(array('Element_OphTrOperationnote_Trabectome' =>
 						array(
@@ -23,44 +24,22 @@ class m140513_101425_add_trabectome_element extends OEMigration
 			echo "***** WARNING *****\nCreating Trabectome element, but no procedure found to associate with the the element\n";
 		}
 
-		$this->createTable('ophtroperationnote_trabectome_power', array(
+		$this->createOETable('ophtroperationnote_trabectome_power', array(
 						'id' => 'pk',
 						'name' => 'string NOT NULL',
 						'active' => 'boolean NOT NULL DEFAULT true',
 						'display_order' => 'integer NOT NULL',
-						'last_modified_user_id' => 'int(10) unsigned DEFAULT 1',
-						'last_modified_date' => "datetime DEFAULT '1900-01-01 00:00:00'",
-						'created_user_id' => 'int(10) unsigned  DEFAULT 1',
-						'created_date' => "datetime DEFAULT '1900-01-01 00:00:00'"
-				), 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
+				), true);
 
-		$this->addForeignKey('ophtroperationnote_trabectome_power_lmui_fk',
-				'ophtroperationnote_trabectome_power',
-				'last_modified_user_id', 'user', 'id');
-		$this->addForeignKey('ophtroperationnote_trabectome_power_cui_fk',
-				'ophtroperationnote_trabectome_power',
-				'created_user_id', 'user', 'id');
-
-		$this->createTable('ophtroperationnote_trabectome_complication', array(
+		$this->createOETable('ophtroperationnote_trabectome_complication', array(
 						'id' => 'pk',
 						'name' => 'string NOT NULL',
 						'active' => 'boolean NOT NULL DEFAULT true',
 						'other' => 'boolean NOT NULL DEFAULT false',
 						'display_order' => 'integer NOT NULL',
-						'last_modified_user_id' => 'int(10) unsigned DEFAULT 1',
-						'last_modified_date' => "datetime DEFAULT '1900-01-01 00:00:00'",
-						'created_user_id' => 'int(10) unsigned  DEFAULT 1',
-						'created_date' => "datetime DEFAULT '1900-01-01 00:00:00'"
-				), 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
+				), true);
 
-		$this->addForeignKey('ophtroperationnote_trabectome_complication_lmui_fk',
-				'ophtroperationnote_trabectome_complication',
-				'last_modified_user_id', 'user', 'id');
-		$this->addForeignKey('ophtroperationnote_trabectome_complication_cui_fk',
-				'ophtroperationnote_trabectome_complication',
-				'created_user_id', 'user', 'id');
-
-		$this->createTable('et_ophtroperationnote_trabectome', array(
+		$this->createOETable('et_ophtroperationnote_trabectome', array(
 						'id' => 'pk',
 						'event_id' => 'int(10) unsigned NOT NULL',
 						'power_id' => 'integer NOT NULL',
@@ -69,43 +48,27 @@ class m140513_101425_add_trabectome_element extends OEMigration
 						'eyedraw' => 'text',
 						'description' => 'text',
 						'complication_other' => 'text',
-						'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
-						'last_modified_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
-						'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
-						'created_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
-						'KEY `et_ophtroperationnote_trabectome_lmui_fk` (`last_modified_user_id`)',
-						'KEY `et_ophtroperationnote_trabectome_cui_fk` (`created_user_id`)',
-						'KEY `et_ophtroperationnote_trabectome_ev_fk` (`event_id`)',
-						'CONSTRAINT `et_ophtroperationnote_trabectome_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
-						'CONSTRAINT `et_ophtroperationnote_trabectome_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
-						'CONSTRAINT `et_ophtroperationnote_trabectome_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)',
-				), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
+				), true);
+
+		$this->addForeignKey('et_ophtroperationnote_trabectome_ev_fk',
+			'et_ophtroperationnote_trabectome', 'event_id', 'event', 'id');
 
 		$this->addForeignKey('et_ophtroperationnote_trabectome_power_id',
 			'et_ophtroperationnote_trabectome', 'power_id',
 			'ophtroperationnote_trabectome_power', 'id');
 
-		$this->createTable('ophtroperationnote_trabectome_comp_ass', array(
+		$this->createOETable('ophtroperationnote_trabectome_comp_ass', array(
 						'id' => 'pk',
 						'element_id' => 'integer NOT NULL',
 						'complication_id' => 'integer NOT NULL',
-						'last_modified_user_id' => 'int(10) unsigned DEFAULT 1',
-						'last_modified_date' => "datetime DEFAULT '1900-01-01 00:00:00'",
-						'created_user_id' => 'int(10) unsigned  DEFAULT 1',
-						'created_date' => "datetime DEFAULT '1900-01-01 00:00:00'"
-				), 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
+				), true);
 
-		$this->addForeignKey('ophtroperationnote_trabectome_comp_ass_lmui_fk',
+		$this->addForeignKey('ophtroperationnote_trabectome_comp_ass_elui_fk',
 				'ophtroperationnote_trabectome_comp_ass',
-				'last_modified_user_id', 'user', 'id');
-		$this->addForeignKey('ophtroperationnote_trabectome_comp_ass_cui_fk',
+				'element_id', 'et_ophtroperationnote_trabectome', 'id');
+		$this->addForeignKey('ophtroperationnote_trabectome_comp_ass_cmpui_fk',
 				'ophtroperationnote_trabectome_comp_ass',
-				'created_user_id', 'user', 'id');
-
-		$this->versionExistingTable('ophtroperationnote_trabectome_power');
-		$this->versionExistingTable('ophtroperationnote_trabectome_complication');
-		$this->versionExistingTable('et_ophtroperationnote_trabectome');
-		$this->versionExistingTable('ophtroperationnote_trabectome_comp_ass');
+				'complication_id', 'ophtroperationnote_trabectome_complication', 'id');
 
 		$migrations_path = dirname(__FILE__);
 		$this->initialiseData($migrations_path);
@@ -113,17 +76,12 @@ class m140513_101425_add_trabectome_element extends OEMigration
 
 	public function down()
 	{
-		$this->dropTable('ophtroperationnote_trabectome_comp_ass_version');
-		$this->dropTable('et_ophtroperationnote_trabectome_version');
-		$this->dropTable('ophtroperationnote_trabectome_complication_version');
-		$this->dropTable('ophtroperationnote_trabectome_power_version');
-		$this->dropTable('ophtroperationnote_trabectome_comp_ass');
-		$this->dropTable('et_ophtroperationnote_trabectome');
-		$this->dropTable('ophtroperationnote_trabectome_complication');
-		$this->dropTable('ophtroperationnote_trabectome_power');
+		$this->dropOETable('ophtroperationnote_trabectome_comp_ass', true);
+		$this->dropOETable('et_ophtroperationnote_trabectome', true);
+		$this->dropOETable('ophtroperationnote_trabectome_complication', true);
+		$this->dropOETable('ophtroperationnote_trabectome_power', true);
 
-		$element_type = $this->dbConnection->createCommand()->select("id")->from("element_type")->where("class_name = ?", array('Element_OphTrOperationnote_Trabectome'))->queryRow();
-		$element_type_id = $element_type['id'];
+		$element_type_id = $this->getIdOfElementTypeByClassName('Element_OphTrOperationnote_Trabectome');
 		$this->delete('ophtroperationnote_procedure_element', 'element_type_id = ?', array($element_type_id));
 		$this->delete('element_type', 'id = ?', array($element_type_id));
 	}
