b8c9889ab9360e0ec15c4d25b9a0e00a309718af
Mark Wadham <m@rkw.me>
2014-06-08 08:57:08 +0000

    fixed xss bug when rendering the surgeon position eyedraw widget.

--- widgets/views/OEEyeDrawWidgetSurgeonPosition.php
+++ widgets/views/OEEyeDrawWidgetSurgeonPosition.php
@@ -19,5 +19,5 @@
 ?>
 <div class="EyeDrawWidget">
 	<canvas id="<?php echo $canvasId?>" class="<?php if ($isEditable) { echo 'edit'; } else { echo 'display'; }?>" width="<?php echo $width?>" height="<?php echo $height?>" tabindex="1"<?php if ($canvasStyle) {?> style="<?php echo $canvasStyle?>"<?php }?>></canvas>
-	<input type="hidden" id="<?php echo $inputId?>" name="<?php echo $inputName?>" value='<?php echo $this->model[$this->attribute]?>' />
+	<?php echo CHtml::hiddenField($inputName,$this->model[$this->attribute])?>
 </div>
