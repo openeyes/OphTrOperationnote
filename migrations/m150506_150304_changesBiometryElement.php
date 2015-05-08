46676e579e81c545a00b4c2756dfae31fc000c3d
vetusko@gmail.com <bazdmeg1>
2015-05-08 16:13:14 +0100

    migration fix in view definition

--- migrations/m150506_150304_changesBiometryElement.php
+++ migrations/m150506_150304_changesBiometryElement.php
@@ -34,19 +34,21 @@ class m150506_150304_changesBiometryElement extends CDbMigration
 			echo "**WARNING** 'Intracapsular cataract extraction' not present in proc table, not linking to element type\n";
 		}
 
-		$this->execute("CREATE OR REPLACE VIEW et_ophtroperationnote_biometry AS
-							SELECT eol.event_id, eol.eye_id, eol.last_modified_date AS last_modified_date, target_refraction_left, target_refraction_right,
+		$this->execute("CREATE OR REPLACE VIEW et_ophtroperationnote_biometry AS SELECT
+							eol.id, eol.eye_id, eol.last_modified_date, target_refraction_left, target_refraction_right,
 							(SELECT name FROM ophinbiometry_lenstype_lens oll WHERE oll.id=lens_id_left) as lens_left,
 							(SELECT description FROM ophinbiometry_lenstype_lens oll WHERE oll.id=lens_id_left) as lens_description_left,
 							(SELECT acon FROM ophinbiometry_lenstype_lens oll WHERE oll.id=lens_id_left) AS lens_acon_left,
 							(SELECT name FROM ophinbiometry_lenstype_lens oll WHERE oll.id=lens_id_right) as lens_right,
 							(SELECT description FROM ophinbiometry_lenstype_lens oll WHERE oll.id=lens_id_right) as lens_description_right,
 							(SELECT acon FROM ophinbiometry_lenstype_lens oll WHERE oll.id=lens_id_right) AS lens_acon_right,
-							k1_left, k1_right, k2_left, k2_right, axis_k1_left, axis_k1_right, axial_length_left, axial_length_right, snr_left,
-							snr_right, iol_power_left, iol_power_right, predicted_refraction_left, predicted_refraction_right
+							k1_left, k1_right, k2_left, k2_right, axis_k1_left, axis_k1_right, axial_length_left, axial_length_right,
+							snr_left, snr_right, iol_power_left, iol_power_right, predicted_refraction_left, predicted_refraction_right, patient_id
 							FROM et_ophinbiometry_lenstype eol
 							JOIN et_ophinbiometry_calculation eoc ON eoc.event_id=eol.event_id
 							JOIN et_ophinbiometry_selection eos ON eos.event_id=eol.event_id
+							JOIN event ev ON ev.id=eol.event_id
+							JOIN episode ep ON ep.id=ev.episode_id
 							ORDER BY eol.last_modified_date;");
 
 	}
