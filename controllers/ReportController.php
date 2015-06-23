db108a86e0f0b3e739eee44a8fedf22434df4b1b
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-06-23 13:59:44 +0100

    OE-5644: Add action for risk report

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -24,7 +24,7 @@ class ReportController extends BaseReportController
 	{
 		return array(
 			array('allow',
-				'actions' => array('index', 'operation', 'runReport', 'downloadReport'),
+				'actions' => array('index', 'operation', 'runReport', 'downloadReport', 'pcrRisk'),
 				'roles' => array('OprnGenerateReport', 'admin'),
 			)
 		);
@@ -281,5 +281,10 @@ class ReportController extends BaseReportController
 		}
 
 		return array('operations'=>$operations);
+	}
+
+	public function actionPcrRisk()
+	{
+		$this->render('//report/pcrrisk', array('chart' => $pcrRiskChart->getChart()));
 	}
 }
